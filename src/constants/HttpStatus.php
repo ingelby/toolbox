efe6d2aeec419e22b637c0416130bc22e71cb780
jiggy <george_hallam@hotmail.com>
2018-07-14 10:55:45 +0100

    - Added extra http Status codes

--- src/constants/HttpStatus.php
+++ src/constants/HttpStatus.php
@@ -6,12 +6,14 @@ class HttpStatus
 {
     const OK = 200;
     const CREATED = 201;
+    const ACCEPTED = 202;
     const NO_CONTENT = 204;
 
     const BAD_REQUEST = 400;
     const UNAUTHORIZED = 401;
     const NOT_FOUND = 404;
     const CONFLICT = 409;
+    const IM_A_TEA_POT = 418;
 
     const INTERNAL_SERVER_ERROR = 500;
     const NOT_IMPLEMENTED = 501;
