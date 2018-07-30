d15e5ff9186e904b16deba6b0b00f066d003a1a4
jiggy <george_hallam@hotmail.com>
2018-07-30 16:50:55 +0100

    - Added FirstOrCreate trait

---
+++ src/traits/FirstOrCreate.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ingelby\toolbox\traits;
+
+
+use yii\caching\TagDependency;
+use yii\db\ActiveRecord;
+
+trait FirstOrCreate
+{
+    protected static $lastErrors = [];
+
+    /**
+     * @return array
+     */
+    public static function getLastErrors()
+    {
+        return static::$lastErrors;
+    }
+
+    /**
+     * @param mixed|array $searchCondition
+     * @param array|null $createParameters
+     * @return ActiveRecord|bool false if unable to create
+     */
+    public static function firstOrCreate($searchCondition, array $createParameters = null)
+    {
+        $model = static::find()->cache(true, new TagDependency(['tags' => static::class]))->where($searchCondition)->one();
+        if (null !== $model) {
+            return $model;
+        }
+
+        if (null === $createParameters) {
+            //This is good for simple things like ['foo' => 'bar']
+            $createParameters = $searchCondition;
+        }
+        $model = new static($createParameters);
+        if (!$model->save()) {
+            \Yii::error('Unable to save model, see next line for error');
+            \Yii::error($model->getErrors());
+            static::$lastErrors = $model->getErrors();
+            return false;
+
+        }
+        TagDependency::invalidate(\Yii::$app->cache, static::class);
+
+        return $model;
+    }
+}
