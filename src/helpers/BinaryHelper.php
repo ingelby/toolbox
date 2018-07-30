ab082f5ba3b42868ecd53550d84ebfe8a5b64ad6
jiggy <george_hallam@hotmail.com>
2018-07-30 10:40:30 +0100

    - Added binary helper to toolbox

---
+++ src/helpers/BinaryHelper.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ingelby\toolbox\helpers;
+
+use yii\helpers\FileHelper;
+
+class BinaryHelper
+{
+    /**
+     * @param string $base64EncodedBinary
+     * @return string
+     * @throws \RuntimeException
+     */
+    public static function getBase64AsBinary($base64EncodedBinary): string
+    {
+        $splitData = explode(',', $base64EncodedBinary);
+        if (2 !== \count($splitData)) {
+            throw new \RuntimeException(
+                'Data not in correct format example would be: "data:image/png;base64,AAAFBfj42Pj4"',
+                0
+            );
+        }
+
+        $binary = base64_decode($splitData[1]);
+        if (false === $binary) {
+            throw new \RuntimeException('Unable to decode binary', 1);
+        }
+
+        return $binary;
+
+    }
+
+    /**
+     * @param string $base64EncodedBinary
+     * @return string
+     * @throws \RuntimeException
+     */
+    public static function getExtension($base64EncodedBinary): string
+    {
+        $regex = '/data:(\w+\/\w+);base64/';
+        preg_match($regex, $base64EncodedBinary, $matches);
+        if (!isset($matches[1])) {
+            throw new \RuntimeException(
+                'Unable to determine mimeType from data, please ensure the base64 encoded file contains the mime ' .
+                'type for example "data:image/png;base64,"',
+                2
+            );
+        }
+
+        $mimeType = $matches[1];
+
+        $extension = FileHelper::getExtensionsByMimeType($mimeType);
+
+        if ([] === $extension) {
+            throw new \RuntimeException(
+                'Unable to determine file extension for mime type: ' . $mimeType,
+                3
+            );
+        }
+
+        return $extension[0];
+    }
+
+}
